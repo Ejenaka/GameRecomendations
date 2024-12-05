fb26777619a64b3cc6e8600370ec2642f593038a
Ejenaka <interpaint1975@gmail.com>
2024-12-05 02:03:30 +0200

    Refactored code: hide steam grid db api key & move not found image to Resources.cs

---
+++ GameRecommendations/Resources.cs
@@ -0,0 +1,6 @@
+namespace GameRecommendations;
+
+public static class Resources
+{
+    public const string ImageNotFoundUrl = "https://static.vecteezy.com/system/resources/previews/005/720/408/original/crossed-image-icon-picture-not-available-delete-picture-symbol-free-vector.jpg";
+}
